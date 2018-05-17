f1d671601f0b68f177e9390a805be5b51d44b350
William <menzongwei@126.com>
2018-05-17 17:36:48 +0800

    add data,optimize toggle function

--- src/assets/components/demo20.vue
+++ src/assets/components/demo20.vue
@@ -1,20 +1,39 @@
 <template>
+<div>
     <h1 :style="{opacity:opa}">this is an example of life-circle</h1>
+    <button @click="handleClick">clear</button>
+</div>
 </template>
 <script>
     export default{
         data(){
             return{
-               opa:0
+               opa:0,
+               timer:null,
+               isChange:true
             }
         },
         mounted(){
-            let timer = setInterval(()=>{
+            this.timer = setInterval(()=>{
                 this.opa+=0.1;
             },500)
         },
         updated(){
             this.opa=this.opa>1?0:this.opa;
+        },
+        methods:{
+            handleClick(){
+                if(this.isChange){
+                    clearInterval(this.timer);
+                    this.isChange = false;
+                }else{
+                    this.timer = setInterval(()=>{
+                        this.opa+=0.1;
+                    },500);
+                    this.isChange = true;
+                }
+                
+            }
         }
     }
 </script>
